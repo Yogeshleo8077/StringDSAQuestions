8e76470b9e1f3c9705794e985ab417505b259060
Yogeshleo8077 <yogeshleo420@gmail.com>
2025-12-02 18:27:49 +0530

    Added Another String Question Solution

---
+++ IsSubString.java
@@ -0,0 +1,29 @@
+public class IsSubString {
+    public static boolean isSubString(String str, String sub) {
+        if (str == null || sub == null) {
+            return false;
+        }
+
+        for (int i = 0; i <= str.length() - sub.length(); i++) {
+            int j;
+            for (j = 0; j < sub.length(); j++) {
+                if (str.charAt(i + j) != sub.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == sub.length()) {
+                return true; // match found
+            }
+        }
+
+        return false; // no match found
+    }
+
+    public static void main(String args[]) {
+        String str = "hello world";
+        String sub = "world";
+
+        System.out.println(isSubString(str, sub));
+    }
+}
